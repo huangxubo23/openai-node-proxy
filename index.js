82f3c12e38931beae58313e298b2feb0fef66599
Harry Huang <732150595@qq.com>
2023-05-25 17:05:29 +0800

    add test page

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
+const { PORT } = require('./config')
 
 const app = express();
 
-app.use('/v1', createProxyMiddleware({
+// 设置静态资源目录
+app.use(express.static(path.join(__dirname, 'public')))
+
+// 设置openai代理设置
+app.use('/v1/', createProxyMiddleware({
   target: 'https://api.openai.com',
   changeOrigin: true,
-  // onProxyReq: (proxyReq, req, res) => {
-  //   // 添加 OpenAI API 密钥到请求头
-  //   proxyReq.setHeader('Authorization', `Bearer ${process.env.OPENAI_API_KEY}`);
-  // }
+  onProxyReq: (proxyReq, req, res) => {
+    console.log(req.originalUrl);
+    // 添加 OpenAI API 密钥到请求头，这里默认让请求头携带
+    // proxyReq.setHeader('Authorization', `Bearer ${process.env.OPENAI_API_KEY}`);
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    proxyRes.headers["Access-Control-Allow-Headers"] =
+      "Content-Type,Content-Length, Authorization, Accept,X-Requested-With";
+  },
 }));
 
-app.listen(8090, () => {
-  console.log('Proxy server is listening on port 8090');
-});
+// 设置监听端口
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on http://localhost:${PORT}`);
+}).on('error', (err) => {
+  console.error(err)
+})
